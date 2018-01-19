36f37930bb13391cd9ede6b79512bb45b206b052
rbkprod <rbkprod@gmail.com>
2018-01-19 07:37:41 +0200

    20180119
    Fixed run_warnings.py

--- run_warnings.py
+++ run_warnings.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from statusbot import process_warnings
-#process_warnings.run()
-process_warnings.report_warnings()
+process_warnings.run()
+#process_warnings.report_warnings()
